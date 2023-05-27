0afba68d464b71a6402a74024157ee6bad1e3979
DanielWPitsch <102161140+DanielWPitsch@users.noreply.github.com>
2023-05-27 18:08:36 -0300

    Revert "Correcao de bugs"
    This reverts commit 3201e34aaa8948ab7d25b9affa08f2f550816b33.

--- BancoPotencia/src/bancopotencia/Conta.java
+++ BancoPotencia/src/bancopotencia/Conta.java
@@ -1,6 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package bancopotencia;
 
 import java.util.Date;
+
+/**
+ *
+ * @author Elder
+ */
 public class Conta {
     private String idConta;
 
