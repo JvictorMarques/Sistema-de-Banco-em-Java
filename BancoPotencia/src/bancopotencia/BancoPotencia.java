3201e34aaa8948ab7d25b9affa08f2f550816b33
JvictorMarques <jvictorsilvamarques358@gmail.com>
2023-05-27 17:05:10 -0300

    Correcao de bugs

--- BancoPotencia/src/bancopotencia/BancoPotencia.java
+++ BancoPotencia/src/bancopotencia/BancoPotencia.java
@@ -1,24 +1,12 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package bancopotencia;
 import telas.telaLogin;
-import javax.swing.JOptionPane;
-/**
- *
- * @author dpits
- */
-public class BancoPotencia {
 
-    /**
-     * @param args the command line arguments
-     */
+public class BancoPotencia {
     public static void main(String[] args) {
-        // TODO code application logic here
+
        telaLogin telaLoginS = new telaLogin();
-        telaLoginS.show();
+        telaLoginS.setVisible(true);
     }
     
 }
