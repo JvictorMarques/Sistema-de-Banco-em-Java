59d457cc191434d172487f1cb1c793dfd1e71a8e
JvictorMarques <jvictorsilvamarques358@gmail.com>
2023-10-19 07:18:11 -0300

    Arrumando o banco

--- banco_potencia.sql
+++ banco_potencia.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.0
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 11-Jun-2023 às 17:39
--- Versão do servidor: 10.4.27-MariaDB
--- versão do PHP: 8.2.0
+-- Tempo de geração: 19/10/2023 às 12:16
+-- Versão do servidor: 10.4.28-MariaDB
+-- Versão do PHP: 8.2.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,7 +24,7 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `banco`
+-- Estrutura para tabela `banco`
 --
 
 CREATE TABLE `banco` (
@@ -33,17 +33,10 @@ CREATE TABLE `banco` (
   `localidade` varchar(500) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Extraindo dados da tabela `banco`
---
-
-INSERT INTO `banco` (`id_banco`, `agencia`, `localidade`) VALUES
-(1, 1, 'VIRTUAL');
-
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `cliente`
+-- Estrutura para tabela `cliente`
 --
 
 CREATE TABLE `cliente` (
@@ -51,21 +44,15 @@ CREATE TABLE `cliente` (
   `id_banco` varchar(500) DEFAULT NULL,
   `cpf` varchar(500) DEFAULT NULL,
   `cnpj` varchar(500) DEFAULT NULL,
-  `data_criacao` datetime DEFAULT current_timestamp()
+  `data_criacao` datetime DEFAULT current_timestamp(),
+  `Pais` varchar(3) DEFAULT 'BRA',
+  `Email` varchar(30) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Extraindo dados da tabela `cliente`
---
-
-INSERT INTO `cliente` (`id_cliente`, `id_banco`, `cpf`, `cnpj`, `data_criacao`) VALUES
-(24, '1', '05528832470', NULL, '2023-06-09 14:31:32'),
-(25, '1', '5065406540', NULL, '2023-06-10 16:33:44');
 
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `contacorrente`
+-- Estrutura para tabela `contacorrente`
 --
 
 CREATE TABLE `contacorrente` (
@@ -78,18 +65,10 @@ CREATE TABLE `contacorrente` (
   `data_criacao` datetime DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Extraindo dados da tabela `contacorrente`
---
-
-INSERT INTO `contacorrente` (`id_conta_corrente`, `id_cliente`, `saldo`, `taxa`, `saldo_limite`, `senha`, `data_criacao`) VALUES
-(55800, 24, 2649, 0, 0, '123456', '2023-06-09 14:31:32'),
-(55801, 25, 69914, 0, 0, '123456', '2023-06-10 16:33:44');
-
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `contapoupanca`
+-- Estrutura para tabela `contapoupanca`
 --
 
 CREATE TABLE `contapoupanca` (
@@ -99,18 +78,10 @@ CREATE TABLE `contapoupanca` (
   `data_criacao` datetime DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Extraindo dados da tabela `contapoupanca`
---
-
-INSERT INTO `contapoupanca` (`id_conta_poupanca`, `id_conta_corrente`, `rendimentos`, `data_criacao`) VALUES
-(8, 55800, 639, '2023-06-09 14:31:32'),
-(9, 55801, 87340, '2023-06-10 16:33:44');
-
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `contato`
+-- Estrutura para tabela `contato`
 --
 
 CREATE TABLE `contato` (
@@ -122,18 +93,10 @@ CREATE TABLE `contato` (
   `data_criacao` datetime DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Extraindo dados da tabela `contato`
---
-
-INSERT INTO `contato` (`id_contato`, `id_conta_corrente`, `email`, `contato1`, `contato2`, `data_criacao`) VALUES
-(6, 55800, '654654', '654', '654', '2023-06-09 14:31:32'),
-(7, 55801, 'elder@email.com', '(83)986290077', '(83)698476546', '2023-06-10 16:33:44');
-
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `endereco`
+-- Estrutura para tabela `endereco`
 --
 
 CREATE TABLE `endereco` (
@@ -147,18 +110,10 @@ CREATE TABLE `endereco` (
   `data_criacao` datetime DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Extraindo dados da tabela `endereco`
---
-
-INSERT INTO `endereco` (`id_endereco`, `id_conta_corrente`, `logradouro`, `numero`, `cep`, `cidade`, `estado`, `data_criacao`) VALUES
-(6, 55800, 'napoelao', '320', '505', '654654', '654654', '2023-06-09 14:31:32'),
-(7, 55801, 'Napoleão', '320', '58071590', 'João pesso', 'Paraiba', '2023-06-10 16:33:44');
-
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `pessoafisica`
+-- Estrutura para tabela `pessoafisica`
 --
 
 CREATE TABLE `pessoafisica` (
@@ -172,18 +127,10 @@ CREATE TABLE `pessoafisica` (
   `data_criacao` datetime DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Extraindo dados da tabela `pessoafisica`
---
-
-INSERT INTO `pessoafisica` (`id_pessoafisica`, `id_conta_corrente`, `cpf`, `nome`, `sobrenome`, `data_nascimento`, `renda`, `data_criacao`) VALUES
-(3, 55800, '05528832470', 'Elder', 'Elder', '16/08/1990', 0, '2023-06-09 14:31:32'),
-(4, 55801, '5065406540', 'Elder', 'Elder', '16/08/1990', 0, '2023-06-10 16:33:44');
-
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `pessoajuridica`
+-- Estrutura para tabela `pessoajuridica`
 --
 
 CREATE TABLE `pessoajuridica` (
@@ -202,7 +149,7 @@ CREATE TABLE `pessoajuridica` (
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `saque`
+-- Estrutura para tabela `saque`
 --
 
 CREATE TABLE `saque` (
@@ -212,7 +159,21 @@ CREATE TABLE `saque` (
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `transacoes`
+-- Estrutura para tabela `tb_vendedor`
+--
+
+CREATE TABLE `tb_vendedor` (
+  `codigo` int(11) NOT NULL,
+  `nome` varchar(100) NOT NULL,
+  `salario` decimal(8,2) DEFAULT NULL,
+  `telefone` int(11) DEFAULT NULL,
+  `sexo` char(1) DEFAULT NULL
+) ;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `transacoes`
 --
 
 CREATE TABLE `transacoes` (
@@ -220,7 +181,7 @@ CREATE TABLE `transacoes` (
   `id_conta_destinario` int(50) DEFAULT NULL,
   `id_conta_emissor` int(50) DEFAULT NULL,
   `id_conta_poupanca` int(50) DEFAULT NULL,
-  `tipo_transacao` varchar(500) DEFAULT NULL,
+  `tipo_transacao` varchar(50) DEFAULT NULL,
   `credito_debito` varchar(500) DEFAULT NULL,
   `valor_transacao` double NOT NULL,
   `data_criacao` datetime DEFAULT current_timestamp()
@@ -231,61 +192,67 @@ CREATE TABLE `transacoes` (
 --
 
 --
--- Índices para tabela `banco`
+-- Índices de tabela `banco`
 --
 ALTER TABLE `banco`
   ADD PRIMARY KEY (`id_banco`);
 
 --
--- Índices para tabela `cliente`
+-- Índices de tabela `cliente`
 --
 ALTER TABLE `cliente`
   ADD PRIMARY KEY (`id_cliente`);
 
 --
--- Índices para tabela `contacorrente`
+-- Índices de tabela `contacorrente`
 --
 ALTER TABLE `contacorrente`
   ADD PRIMARY KEY (`id_conta_corrente`);
 
 --
--- Índices para tabela `contapoupanca`
+-- Índices de tabela `contapoupanca`
 --
 ALTER TABLE `contapoupanca`
   ADD PRIMARY KEY (`id_conta_poupanca`);
 
 --
--- Índices para tabela `contato`
+-- Índices de tabela `contato`
 --
 ALTER TABLE `contato`
   ADD PRIMARY KEY (`id_contato`);
 
 --
--- Índices para tabela `endereco`
+-- Índices de tabela `endereco`
 --
 ALTER TABLE `endereco`
   ADD PRIMARY KEY (`id_endereco`);
 
 --
--- Índices para tabela `pessoafisica`
+-- Índices de tabela `pessoafisica`
 --
 ALTER TABLE `pessoafisica`
   ADD PRIMARY KEY (`id_pessoafisica`);
 
 --
--- Índices para tabela `pessoajuridica`
+-- Índices de tabela `pessoajuridica`
 --
 ALTER TABLE `pessoajuridica`
   ADD PRIMARY KEY (`id_pessoajuridica`);
 
 --
--- Índices para tabela `transacoes`
+-- Índices de tabela `tb_vendedor`
+--
+ALTER TABLE `tb_vendedor`
+  ADD PRIMARY KEY (`codigo`);
+
+--
+-- Índices de tabela `transacoes`
 --
 ALTER TABLE `transacoes`
   ADD PRIMARY KEY (`id_transferencia`);
 
 --
--- AUTO_INCREMENT de tabelas despejadas
+-- AUTO_INCREMENT para tabelas despejadas
 --
 
 --
@@ -298,37 +265,37 @@ ALTER TABLE `banco`
 -- AUTO_INCREMENT de tabela `cliente`
 --
 ALTER TABLE `cliente`
-  MODIFY `id_cliente` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
+  MODIFY `id_cliente` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
 
 --
 -- AUTO_INCREMENT de tabela `contacorrente`
 --
 ALTER TABLE `contacorrente`
-  MODIFY `id_conta_corrente` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=55802;
+  MODIFY `id_conta_corrente` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=55803;
 
 --
 -- AUTO_INCREMENT de tabela `contapoupanca`
 --
 ALTER TABLE `contapoupanca`
-  MODIFY `id_conta_poupanca` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id_conta_poupanca` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT de tabela `contato`
 --
 ALTER TABLE `contato`
-  MODIFY `id_contato` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id_contato` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT de tabela `endereco`
 --
 ALTER TABLE `endereco`
-  MODIFY `id_endereco` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id_endereco` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT de tabela `pessoafisica`
 --
 ALTER TABLE `pessoafisica`
-  MODIFY `id_pessoafisica` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id_pessoafisica` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT de tabela `pessoajuridica`
@@ -337,10 +304,16 @@ ALTER TABLE `pessoajuridica`
   MODIFY `id_pessoajuridica` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
+-- AUTO_INCREMENT de tabela `tb_vendedor`
+--
+ALTER TABLE `tb_vendedor`
+  MODIFY `codigo` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT de tabela `transacoes`
 --
 ALTER TABLE `transacoes`
-  MODIFY `id_transferencia` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=45;
+  MODIFY `id_transferencia` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=48;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
